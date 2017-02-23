8ec857c82cbdf6344a0a256bbf333da06d5483a2
SirBlackser <dries.blontrock@student.uantwerpen.be>
2017-02-23 10:26:21 +0100

    hiervan beginnen

---
+++ src/main/java/be/uantwerpen/be/Pgo2Application.java
@@ -0,0 +1,12 @@
+package be.uantwerpen.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pgo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pgo2Application.class, args);
+	}
+}
