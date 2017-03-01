d579291a26df545b5ce511e7542cead969711fb0
Athrun2ala <oliver.nyssen@student.uantwerpen.Be>
2017-03-01 18:05:27 +0100

    Update:
    Grafiek werkt (url moet nog aangepast worden van zodra deze beschikbaar is op de server)
    
    update 2: stats werkt (met navbar voor extra pagina)
    
    TE DOEN: urls aanpassen zodat die naar de server kunnen babbelen (momenteel gelinkt naar voorbeelden op myjson.com)

---
+++ src/main/java/be/uantwerpen/Controller/StatsController.java
@@ -0,0 +1,18 @@
+package be.uantwerpen.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Revil on 01/03/2017.
+ */
+@Controller
+//@RestController
+public class StatsController {
+    @RequestMapping({"/Stats"})
+    public String showHomepage(ModelMap model)
+    {
+        return "StatsPage";
+    }
+}
